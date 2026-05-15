161311c88991931793415c9354786291702b501c
Rohit Singh Pokhariya <rohit.pokhariya123@gmail.com>
2026-05-15 19:00:06 +0530

    implement cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,22 @@
 const express = require("express")
+const cors = require("cors");
 const cookieParser = require("cookie-parser")
 const authRouter = require("./routes/auth.route")
 const profileRoutes = require("./routes/profile.routes");
 const projectRoutes = require("./routes/project.routes");
 const blogRoutes = require("./routes/blog.routes");
 
+
+
 const app = express()
 
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+  origin: "http://localhost:5173/",
+  credentials: true
+}))
 
 app.use("/api/auth", authRouter)
 app.use("/api/profiles", profileRoutes);
